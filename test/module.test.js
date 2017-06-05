661c88e3601bd2cbae42b43d2612a2f3e2b8dd76
Mathew Cormier <mathew.corm@gmail.com>
2017-06-04 22:04:42 -0400

    Using arrow functions in module test + test stand-alone Currencies

--- test/module.test.js
+++ test/module.test.js
@@ -1,16 +1,22 @@
-var Money = require('../build/').Money;
+let { Money, Currencies } = require('../build/')
 
-describe('js-money', function() {
 
-    it('should export constructor directly from package', function() {
+describe('js-money', () => {
+
+    it('should export Money constructor', () => {
         expect(Money).to.be.a('function');
-    });
+    })
 
-    it('should export currencies', function() {
+    it('should export currencies', () => {
         expect(Money.EUR).to.be.a('object');
-    });
+    })
 
-    it('should export factory methods', function() {
+    it('should export factory methods', () => {
         expect(Money.fromDecimal).to.be.a('function');
-    });
-});
+    })
+
+    it('should export stand-alone Currencies object', () => {
+        expect(Currencies).to.be.a('object')
+        expect(Currencies.EUR).to.be.a('object')
+    })
+})
