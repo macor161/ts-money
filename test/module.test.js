924982de1d4c8b467f85f07737925e8a836bfdcd
Mathew Cormier <mathew.corm@gmail.com>
2017-06-04 22:35:13 -0400

    Remove semicolons from module test

--- test/module.test.js
+++ test/module.test.js
@@ -4,15 +4,15 @@ let { Money, Currencies } = require('../build/')
 describe('js-money', () => {
 
     it('should export Money constructor', () => {
-        expect(Money).to.be.a('function');
+        expect(Money).to.be.a('function')
     })
 
     it('should export currencies', () => {
-        expect(Money.EUR).to.be.a('object');
+        expect(Money.EUR).to.be.a('object')
     })
 
     it('should export factory methods', () => {
-        expect(Money.fromDecimal).to.be.a('function');
+        expect(Money.fromDecimal).to.be.a('function')
     })
 
     it('should export stand-alone Currencies object', () => {
